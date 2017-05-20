002e34bc394619f7a0bf71420ec9f512d0341e82
bin <bin_job@sian.com>
2017-05-20 17:16:39 +0800

    第一次提交

---
+++ src/com/ejavashop/TestBook.java
@@ -0,0 +1,12 @@
+package com.ejavashop;
+
+public class TestBook {
+	public static void main(String[] args) {
+		System.out.println("helloword");
+		System.out.println(args.length);
+		for (String string : args) {
+			System.out.println(string);
+		}
+		System.out.println("不一样的烟火");
+	}
+}
